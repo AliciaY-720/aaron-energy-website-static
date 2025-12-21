2fd377da678ff6390a8f8c646c02194282cf071d
Beixuan Yang <yangbeixuan@gmail.com>
2025-12-21 10:14:44 -0500

    Update equipment rental service details: change icon to lightning-charge and update title to Electrical

--- astro.config.mjs
+++ astro.config.mjs
@@ -16,7 +16,7 @@ export default defineConfig({
         // Include Bootstrap icons used in the project
         bi: ['journal-check', 'play-fill', 'egg-fried', 'gem', 'cup-hot', 'star-half', 'flower1', 'heart-fill', 'clock-fill', 'whatsapp', 'chevron-down', 'send', 'arrow-up', 'play-circle-fill', 'building', 'minecart-loaded', 'fuel-pump', 'houses', 'clock-history', 'truck-front', 'star-fill', 'chevron-left', 'chevron-right', 'person-check', 'calendar-check', 'eye', 'flag', 'geo-alt-fill', 'send-fill',
           'list', 'tiktok', 'x-lg', 'envelope', 'telephone', 'geo-alt', 'tag', 'shield-check', 'cash-stack', 'house-door', 'graph-up-arrow', 'info-circle', 'headset', 'graph-up', 'currency-dollar', 'people', 'award', 'github', 'youtube', 'google', 'arrow-right', 'palette-fill', 'bar-chart-fill', 'search', 'envelope-fill', 'facebook', 'instagram', 'twitter-x', 'linkedin', 'telephone-fill', 'braces', 'award-fill',
-          'truck', 'check-circle-fill', 'speedometer2', 'calculator', 'cone-striped', 'house-door-fill', 'gear-fill', 'folder-fill', 'arrow-left', 'arrow-right', 'layers', 'wrench', 'person', 'clipboard', 'shield', 'wechat', 'box-arrow-up-right'
+          'truck', 'check-circle-fill', 'speedometer2', 'calculator', 'cone-striped', 'house-door-fill', 'gear-fill', 'folder-fill', 'arrow-left', 'arrow-right', 'layers', 'wrench', 'person', 'clipboard', 'shield', 'wechat', 'box-arrow-up-right', 'lightning-charge',
         ],
       }
     }),
